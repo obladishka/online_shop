1a1a07c6b8f51e18cb660e89bb3edffd41f8e9e7
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-15 22:30:46 +0300

    add fixtures for testing

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture
+def product_1():
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture
+def product_2():
+    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+
+@pytest.fixture
+def smartphones():
+    return Category("Smartphones", "Smartphones that make your life better", [
+        Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+        Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    ])
