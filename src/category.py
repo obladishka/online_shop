c471585fcdd46192362dbde760cca50c1ae9dc36
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-18 14:53:18 +0300

    add method for Products adding

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Class for creating product categories."""
 
@@ -11,7 +14,11 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        if type(product) is Product:
+            self.__products.append(product)
