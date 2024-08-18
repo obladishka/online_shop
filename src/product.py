5da9cb752b8fbbef0eedd55062f7400256b44d26
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-18 16:46:26 +0300

    add classmethod for new objs creation

--- src/product.py
+++ src/product.py
@@ -6,8 +6,24 @@ class Product:
     price: float
     quantity: int
 
+    products = []
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+        self.products.append(self)
+
+    @classmethod
+    def new_product(cls, product: dict):
+        if sorted(product.keys()) == ["description", "name", "price", "quantity"]:
+            if any(obj.name == product.get("name") for obj in cls.products):
+                for obj in cls.products:
+                    if product.get("name") == obj.name and product.get("price") > obj.price:
+                        obj.price = product.get("price")
+                        obj.quantity += product.get("quantity")
+                    elif product.get("name") == obj.name:
+                        obj.quantity += product.get("quantity")
+            else:
+                return cls(**product)
