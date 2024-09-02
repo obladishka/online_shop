d4925f1e6c6b34285d840546bb2b31979fc46f98
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-09-02 21:04:21 +0300

    add tests for printing mixin

--- src/print_mixin.py
+++ src/print_mixin.py
@@ -3,4 +3,4 @@ class PrintMixin:
 
     def __repr__(self):
         """Method for message creation and printing."""
-        print(f"{self.__class__.__name__}({*[value for value in self.__dict__.values()], })")
+        print(f"{self.__class__.__name__}{*[value for value in self.__dict__.values()], }")
